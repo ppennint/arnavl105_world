7a756b01fa63bbcfb59465f512ab7c1e94ac4875
Pooja Penninti <ppennint@andrew.cmu.edu>
2016-04-27 12:36:00 -0400

    added model code

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ActiveRecord::Base
+	
+
+	belongs_to :category
+
+	validates_prescense_of :title, :content
+
+
+  scope :active, where('active = ?', true)
+  scope :alphabetical, order('title')
+end
